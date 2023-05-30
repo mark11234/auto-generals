658edced732c6e84583ec84b90ad083755dfddc2
mark11234 <m_roberts99@icloud.com>
2023-05-30 14:53:15 +0100

    Minor updates

--- src/helpers/getNextGameState.ts
+++ src/helpers/getNextGameState.ts
@@ -4,16 +4,18 @@ import { BotOutput } from "../models/botOutput";
 import { verifyMove } from "./verifyMove";
 
 // Change these to change the bots
-import bot1 from '../bots/nullBot';
-import bot2 from '../bots/nullBot';
+import bot1 from '../bots/snakeBot';
+import bot2 from '../bots/snakeBot';
 
 export const  getNextGameState =  (gameState: GameState): GameState => {
     const nextGameState:GameState={...gameState, turn: gameState.turn + 1};
 
-    const bot1Move = verifyMove(bot1.makeMove(gameState), gameState, 1);
-    doMove(nextGameState, bot1Move, 1);
-    const bot2Move = verifyMove(bot2.makeMove(gameState), gameState, 2);
-    doMove(nextGameState, bot2Move, 2);
+    for (let i = 0; i<2; i++) {
+        const bot1Move = verifyMove(bot1.makeMove(gameState, 1), gameState, 1);
+        doMove(nextGameState, bot1Move, 1);
+        const bot2Move = verifyMove(bot2.makeMove(gameState, 2), gameState, 2);
+        doMove(nextGameState, bot2Move, 2);
+    }
 
     if (nextGameState.turn % 25 === 0) {
         return {
